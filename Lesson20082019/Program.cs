16e68b6d571ad89eeb4683b2d26c16341e520b2a
Aleksandr Grinchenko <sasha_grinchenko@outlook.com>
2019-08-20 21:30:39 +0300

    project with comments lines

--- Lesson20082019/Program.cs
+++ Lesson20082019/Program.cs
@@ -24,65 +24,49 @@ namespace Lesson20082019
                 citiesNames[i]=t[i].Substring(0, t[i].IndexOf('='));//take a substring before the symbol =
             }
 
-            int [] citiesPopulation = new int[t.Length];
+            int [] citiesPopulation = new int[t.Length]; //create array with cities populations
 
             for (int i = 0; i < t.Length; i++)
             {
-                citiesPopulation[i] = int.Parse(t[i].Split('=', ',')[1]);
-            }
+                citiesPopulation[i] = int.Parse(t[i].Split('=', ',')[1]); //take a substring between the symbols = and , convert it to int
+            }            
 
-            //foreach (int item in citiesPopulation)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            int[] citiesArea = new int[t.Length];
+            int[] citiesArea = new int[t.Length]; //create area with cities area
 
             for (int i = 0; i < t.Length; i++)
             {
-                citiesArea[i] = int.Parse(t[i].Split(',')[1]);
-            }
+                citiesArea[i] = int.Parse(t[i].Split(',')[1]);//take a substring after , convert it to int
+            }                       
 
-            //foreach (int item in citiesArea)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            int maxElemMostPopulation = citiesPopulation.Max();
-            int indexElemMostPopulation = citiesPopulation.ToList().IndexOf(maxElemMostPopulation);
-
-           // Console.WriteLine(indexElemMostPopulation);
-
-            Console.WriteLine("Most populated: {0} ({1} people)", citiesNames[indexElemMostPopulation], maxElemMostPopulation);
+            int maxElemMostPopulation = citiesPopulation.Max(); //find the element with the most value of population
+            int indexElemMostPopulation = citiesPopulation.ToList().IndexOf(maxElemMostPopulation);//find the index of element with the most value of population
 
 
-            int [] citiesNamesLength = new int[t.Length];
+            Console.WriteLine("Most populated: {0} ({1} people)", citiesNames[indexElemMostPopulation], maxElemMostPopulation);//make first string to output
+
+
+            int [] citiesNamesLength = new int[t.Length]; //create array with lenghts of cities names
 
             for (int i = 0; i < t.Length; i++)
             {
                 citiesNamesLength[i] = t[i].Length;
             }
+            
 
-            //foreach (int item in citiesNamesLength)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            int maxElemCitiesNamesLength = citiesNamesLength.Max();//find the element with the most value of lenght cities name
+            int indexElemCitiesNamesLength = citiesNamesLength.ToList().IndexOf(maxElemCitiesNamesLength);//find the index of element with the most value of lenght cities name
 
-            int maxElemCitiesNamesLength = citiesNamesLength.Max();
-            int indexElemCitiesNamesLength = citiesNamesLength.ToList().IndexOf(maxElemCitiesNamesLength);
 
-            // Console.WriteLine(indexElemCitiesNamesLength);
+            Console.WriteLine("Longest name: {0}({1} letters)", citiesNames[indexElemCitiesNamesLength], citiesNamesLength[indexElemCitiesNamesLength]);//make second string to output
 
-            Console.WriteLine("Longest name: {0}({1} letters)", citiesNames[indexElemCitiesNamesLength], citiesNamesLength[indexElemCitiesNamesLength]);
+            Console.WriteLine("Density:"); //make third string to output
 
-            Console.WriteLine("Density:");
-
-            for (int i = 0; i < t.Length; i++)
+            for (int i = 0; i < t.Length; i++) //make fourth string to output
             {
-                Console.WriteLine("{0} - {1}", citiesNames[i], Math.Round((Convert.ToDouble(citiesPopulation[i]) / Convert.ToDouble(citiesArea[i])),2) );
+                Console.WriteLine("{0} - {1}", citiesNames[i], Math.Round((Convert.ToDouble(citiesPopulation[i]) / Convert.ToDouble(citiesArea[i])),2) );//in this line make value of Density and convert it to double
             }
 
-            Console.ReadLine();
+            Console.ReadLine();//wait input user
 
         }
     }
