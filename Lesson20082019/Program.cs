ec1ebe65c5a584aa386364f51f7cdf6dd096eeda
Aleksandr Grinchenko <sasha_grinchenko@outlook.com>
2019-08-20 19:52:25 +0300

    Add project files.

---
+++ Lesson20082019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson20082019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
